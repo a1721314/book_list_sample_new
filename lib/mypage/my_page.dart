8a797b319fb47da87a6dce4b02bb9b7944b33fa7
t.nagano.g <t.nagano.g@gmail.com>
2022-01-13 07:25:34 +0900

    ログアウト後にログインページに戻る処理の実装

--- lib/mypage/my_page.dart
+++ lib/mypage/my_page.dart
@@ -1,10 +1,10 @@
 import 'package:book_list_sample_new/edit_profile/edit_profile_page.dart';
+import 'package:book_list_sample_new/login/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'my_model.dart';
 
 class MyPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<MyModel>(
@@ -12,22 +12,22 @@ class MyPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text('マイページ'),
-          actions:[
-            Consumer<MyModel>(builder:(context, model, child) {
+          actions: [
+            Consumer<MyModel>(builder: (context, model, child) {
               return IconButton(
-              onPressed: () async {
-              //画面遷移
-              await Navigator.push(
-              context,
-              MaterialPageRoute(
-              builder: (context) => EditProfilePage(model.name!, model.descrition!),
-              ),
-              );
-              model.fetchUser();
-              },
-              icon: Icon(Icons.edit)
-              );
-              }),
+                  onPressed: () async {
+                    //画面遷移
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            EditProfilePage(model.name!, model.descrition!),
+                      ),
+                    );
+                    model.fetchUser();
+                  },
+                  icon: Icon(Icons.edit));
+            }),
           ],
         ),
         body: Center(
@@ -36,36 +36,42 @@ class MyPage extends StatelessWidget {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Column(children:[
-                    Text(
-                        model.name ?? '名前なし',
-                        style:TextStyle(
+                  child: Column(children: [
+                    Text(model.name ?? '名前なし',
+                        style: TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.bold,
-                        )
-                    ),
+                        )),
                     Text(model.email ?? 'メールアドレスなし'),
                     Text(model.descrition ?? '自己紹介なし'),
-                    TextButton(onPressed: (){
-                      // ログアウト
-                      model.logout();
-                      Navigator.of(context).pop();
-                    },
-                      child: Text('ログアウト'),)
+                    TextButton(
+                      onPressed: () async {
+                        // ログアウト
+                        model.logout();
+                        //Navigator.of(context).pop();
+                        await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                            LoginPage(),
+                      ),
+                    );
+                      },
+                      child: Text('ログアウト'),
+                    )
                   ]),
                 ),
-                if(model.isLoading)
+                if (model.isLoading)
                   Container(
-                    color:Colors.black54,
+                    color: Colors.black54,
                     child: Center(
-                      child:CircularProgressIndicator(),
+                      child: CircularProgressIndicator(),
                     ),
                   ),
               ],
             );
           }),
         ),
-
       ),
     );
   }
