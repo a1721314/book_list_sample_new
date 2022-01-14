d4ba8f27af61a879aed5818fe18d157d09d448a8
t.nagano.g <t.nagano.g@gmail.com>
2022-01-14 23:48:21 +0900

    ログイン状態保持処理追加

--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -5,6 +5,7 @@ import 'package:book_list_sample_new/book_list/book_list_model.dart';
 import 'package:book_list_sample_new/domain/book.dart';
 import 'package:book_list_sample_new/edit_book/edit_book_page.dart';
 import 'package:book_list_sample_new/login/login_page.dart';
+import 'package:book_list_sample_new/main.dart';
 import 'package:book_list_sample_new/mypage/my_model.dart';
 import 'package:book_list_sample_new/mypage/my_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -15,101 +16,98 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 
-
 class BookListPage extends StatelessWidget {
-
   // ユーザーIDの取得
   final String uid = FirebaseAuth.instance.currentUser!.uid;
+  //String uid;
   
+  //BookListPage(this.uid, {Key? key}) : super(key: key);
+
   final _tabs = <Tab>[
-    const Tab(text:'自分の本棚'),
-    const Tab(text:'他の人の本棚'),
+    const Tab(text: '自分の本棚'),
+    const Tab(text: '他の人の本棚'),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return  MultiProvider(
-        providers: [
-          ChangeNotifierProvider<BookListModel>(
-            create: (_) => BookListModel()
-              ..fetchBookList(uid:uid,isAllBook: false)
-              ..fetchBookList(uid:uid,isAllBook: true),
-          ),
-        ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<BookListModel>(
+          create: (_) => BookListModel()
+            ..fetchBookList(uid: uid, isAllBook: false)
+            ..fetchBookList(uid: uid, isAllBook: true),
+        ),
+      ],
       child: DefaultTabController(
-        length:_tabs.length,
+        length: _tabs.length,
         child: Scaffold(
           appBar: AppBar(
             title: const Text('本棚'),
-            bottom: TabBar(
-                tabs:_tabs
-            ),
+            bottom: TabBar(tabs: _tabs),
           ),
-
           body: TabBarView(
             children: [
-              TabPage(uid:uid,isAllBook: false),
-              TabPage(uid:uid,isAllBook: true),
+              TabPage(uid: uid, isAllBook: false),
+              TabPage(uid: uid, isAllBook: true),
             ],
           ),
-        bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('マイページ'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            title: Text('本追加'),
-          ),
-        ], fixedColor: Colors.blueAccent,
-          unselectedItemColor: Colors.blueAccent,
-          onTap: (index) async{
-            if(index == 0){
-                //画面遷移
-                if(FirebaseAuth.instance.currentUser != null){
-                  await Navigator.push(
+          bottomNavigationBar: BottomNavigationBar(
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  title: Text('マイページ'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.add),
+                  title: Text('本追加'),
+                ),
+              ],
+              fixedColor: Colors.blueAccent,
+              unselectedItemColor: Colors.blueAccent,
+              onTap: (index) async {
+                if (index == 0) {
+                  //画面遷移
+                  if (FirebaseAuth.instance.currentUser != null) {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MyPage(),
+                      ),
+                    );
+                  } else {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginPage(),
+                      ),
+                    );
+                  }
+                } else {
+                  //画面遷移
+                  final bool? added = await Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => MyPage(),
+                      builder: (context) => AddBookPage(),
+                      fullscreenDialog: true,
                     ),
                   );
-                }else{
-                  await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => LoginPage(),
-                    ),
-                  );
-                }
-            }else{
-                //画面遷移
-                final bool? added = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AddBookPage(),
-                    fullscreenDialog: true,
-                  ),
-                );
 
-                if(added != null && added){
-                  final snackBar = SnackBar(
-                    backgroundColor:Colors.green,
-                    content: Text('本を追加しました'),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
+                  if (added != null && added) {
+                    final snackBar = SnackBar(
+                      backgroundColor: Colors.green,
+                      content: Text('本を追加しました'),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
                   //model.fetchBookList(uid: uid, isAllBook: false);
-            }
-          }
-        ),
+                }
+              }),
         ),
       ),
     );
   }
 
-
-  Future showConfirmDialog(BuildContext context, Book book, model){
+  Future showConfirmDialog(BuildContext context, Book book, model) {
     return showDialog(
       context: context,
       barrierDismissible: false,
@@ -124,12 +122,12 @@ class BookListPage extends StatelessWidget {
             ),
             TextButton(
               child: Text("はい"),
-              onPressed: () async{
+              onPressed: () async {
                 //modelで削除
                 await model.delete(book);
                 Navigator.pop(context);
                 final snackBar = SnackBar(
-                  backgroundColor:Colors.red,
+                  backgroundColor: Colors.red,
                   content: Text('${book.title}を削除しました'),
                 );
                 model.fetchBookList();
@@ -144,7 +142,6 @@ class BookListPage extends StatelessWidget {
 }
 
 class TabPage extends StatelessWidget {
-
   final String uid;
   final bool isAllBook;
 
@@ -158,69 +155,78 @@ class TabPage extends StatelessWidget {
       child: Consumer<BookListModel>(builder: (context, model, child) {
         final List<Book>? books = model.books;
 
-        if(books == null)
-        {
+        if (books == null) {
           return const CircularProgressIndicator();
         }
-        model.fetchBookList(uid:uid,isAllBook: isAllBook);
+        model.fetchBookList(uid: uid, isAllBook: isAllBook);
 
         final List<Widget> widgets = books
             .map(
               (book) => Slidable(
-            child:Container(
-              decoration: const BoxDecoration(
-                  border: Border(bottom: BorderSide(width: 1.0, color: Colors.grey))),
-               child: ListTile(
-                 leading: book.imgURL!= null? Image.network(book.imgURL!, height:60,width:60,fit:BoxFit.cover):Image.network("https://firebasestorage.googleapis.com/v0/b/book-list-sample-fdb8b.appspot.com/o/books%2Fnoimage.png?alt=media&token=f761b0b8-8ce5-4692-a3e8-31492a2df2b1", height:60,width:60,fit:BoxFit.cover),
-                 title: Text(book.title),
-                 subtitle: Text(book.author),
-               ),
-            ),
-            endActionPane: ActionPane(
-              motion: const ScrollMotion(),
-              dismissible: DismissiblePane(onDismissed: () {}),
-              children:  [
-                SlidableAction(
-                  // An action can be bigger than the others.
-                  flex: 2,
-                  backgroundColor: Color(0xFF7BC043),
-                  foregroundColor: Colors.white,
-                  icon: Icons.edit,
-                  label: '編集',
-                  onPressed: (BuildContext context) async {
-                    //編集画面に遷移
-                    final String? title = await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EditBookPage(book),
-                      ),
-                    );
+                child: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 1.0, color: Colors.grey))),
+                  child: ListTile(
+                    leading: book.imgURL != null
+                        ? Image.network(book.imgURL!,
+                            height: 60, width: 60, fit: BoxFit.cover)
+                        : Image.network(
+                            "https://firebasestorage.googleapis.com/v0/b/book-list-sample-fdb8b.appspot.com/o/books%2Fnoimage.png?alt=media&token=f761b0b8-8ce5-4692-a3e8-31492a2df2b1",
+                            height: 60,
+                            width: 60,
+                            fit: BoxFit.cover),
+                    title: Text(book.title),
+                    subtitle: Text(book.author),
+                  ),
+                ),
+                endActionPane: ActionPane(
+                  motion: const ScrollMotion(),
+                  dismissible: DismissiblePane(onDismissed: () {}),
+                  children: [
+                    SlidableAction(
+                      // An action can be bigger than the others.
+                      flex: 2,
+                      backgroundColor: Color(0xFF7BC043),
+                      foregroundColor: Colors.white,
+                      icon: Icons.edit,
+                      label: '編集',
+                      onPressed: (BuildContext context) async {
+                        //編集画面に遷移
+                        final String? title = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => EditBookPage(book),
+                          ),
+                        );
 
-                    if(title != null){
-                      final snackBar = SnackBar(
-                        backgroundColor:Colors.green,
-                        content: Text('$titleを編集しました'),
-                      );
-                    }
-                    //model.fetchBookList(uid:uid,isAllBook: false);
-                  },
+                        if (title != null) {
+                          final snackBar = SnackBar(
+                            backgroundColor: Colors.green,
+                            content: Text('$titleを編集しました'),
+                          );
+                        }
+                        //model.fetchBookList(uid:uid,isAllBook: false);
+                      },
+                    ),
+                    SlidableAction(
+                      backgroundColor: Color(0xFF0392CF),
+                      foregroundColor: Colors.white,
+                      icon: Icons.delete,
+                      label: '削除',
+                      onPressed: (BuildContext context) async {
+                        //削除しますか？って聞いて、はいだったら削除
+                        await bookListPage.showConfirmDialog(
+                            context, book, model);
+                      },
+                    ),
+                  ],
                 ),
-                SlidableAction(
-                  backgroundColor: Color(0xFF0392CF),
-                  foregroundColor: Colors.white,
-                  icon: Icons.delete,
-                  label: '削除',
-                  onPressed: (BuildContext context) async{
-                    //削除しますか？って聞いて、はいだったら削除
-                    await bookListPage.showConfirmDialog(context,book,model);
-                  },
-                ),
-              ],
-            ),
-          ),
-        ).toList();
+              ),
+            )
+            .toList();
         return ListView(
-          children:widgets,
+          children: widgets,
         );
       }),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,94 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'book_list/book_list_page.dart';
 import 'login/login_page.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
+//void main() async{
+void main() async {
+  //WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
 
+class UserState extends ChangeNotifier{
+  User? user;
+  
+  void setUser(User currentUser){
+    if(user == null){
+      //do nothing
+    }
+    else{
+      user = currentUser;
+      notifyListeners();
+      }
+    }
+}
+
 class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  final UserState user = UserState();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginPage(),
+    return ChangeNotifierProvider<UserState>.value(
+        value: user,
+        child: MaterialApp(
+          //デバックラベル非表示
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: LoginCheck(),
+          routes:<String, WidgetBuilder>{
+            "/login":(BuildContext context) => LoginPage(),
+            "/home":(BuildContext context) => BookListPage(),
+          },
+        )
     );
   }
 }
 
+class LoginCheck extends StatefulWidget{
+  LoginCheck({Key? key}) : super(key: key);
+
+  @override
+  _LoginCheckState createState() => _LoginCheckState();
+
+}
+
+class _LoginCheckState extends State<LoginCheck>{
+   //ログイン状態のチェック(非同期で行う)
+  void checkUser() async{
+    final currentUser = FirebaseAuth.instance.currentUser;
+    final userState = Provider.of<UserState>(context,listen: false);
+    if(currentUser == null){
+      Navigator.pushReplacementNamed(context,"/login");
+    }else{
+      userState.setUser(currentUser);
+      Navigator.pushReplacementNamed(context, "/home");
+    }
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    checkUser();
+  }
+  //ログイン状態のチェック時はこの画面が表示される
+  //チェック終了後にホーム or ログインページに遷移する
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text("Loading..."),
+        ),
+      ),
+    );
+  }
+}
