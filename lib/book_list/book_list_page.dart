5babda51c505851e51163f511bb26c994d8748c9
t.nagano.g <t.nagano.g@gmail.com>
2022-01-19 23:31:19 +0900

    本追加蘭にメモ機能追加

--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -307,7 +307,7 @@ class _BookDescription extends StatelessWidget {
           const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
           
           Text(
-            memo!= null ? 'memo:$memo': "",
+            memo!= null ? '$memo': "",
             style: const TextStyle(fontSize: 10.0),
           ),
         ],
